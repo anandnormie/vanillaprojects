abef0be187472be4b5b449bf85a0d0b86228f2b6
Anand Tiwari <102672482+anandnormie@users.noreply.github.com>
2022-08-21 11:37:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 const hourEl = document.getElementById("hours");
 const minEl = document.getElementById("mins");
-const secondsEl = document.getElementById("seconds");
 const ampmel = document.getElementById("ampm");
 
 function updateClock() {
     let h = new Date().getHours();
     let m = new Date().getMinutes();
-    let s = new Date().getSeconds();
-
     let ampm = "AM";
 
     if (h >= 12) {
@@ -17,7 +14,6 @@ function updateClock() {
 
     hourEl.innerText = h;
     minEl.innerText = m;
-    secondsEl.innertext = s;
     ampmel.innertext = ampm;
 }
 updateClock();
